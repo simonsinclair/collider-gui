e60264c6f5aa524f58bb34ee309505eddc430ae9
Simon Sinclair <simonsinclair@users.noreply.github.com>
2016-08-31 17:01:23 +0100

    Update

--- index.js
+++ index.js
@@ -1,45 +1,133 @@
+// index.js
+//
+
+'use strict';
+
+var pkg = require('./package.json');
+
+// Electron.
 var electron = require('electron');
 var ipcRenderer = electron.ipcRenderer;
+
+var tildify = require('tildify');
+var Vue = require('vue');
 var $ = require('jquery');
 
-// RENDERER EVENTS
+Vue.config.devtools = true;
+Vue.config.debug = true;
+
+
+// STATE
 //
 
-$('#js-open').on('click', function () {
-	ipcRenderer.send('project-open');
-});
+var state = {
+  isRunning: false,
+  isLoaded: false,
 
-$('#js-run-stop').on('click', function () {
-	ipcRenderer.send('project-run-stop');
-});
+  location: '',
+
+  logText: '',
+
+  // 'collider.json' contents.
+  project: {},
+
+  showingNewProjectModal: false,
+
+  workspaceView: 'matter',
+};
 
 
-// MAIN EVENTS
+// VIEWS
 //
 
-var console = '';
+Vue.component('workspace-matter', {
+  template: '#workspace-matter-tpl',
+});
 
-ipcRenderer.on('did-open-project', function (e, args) {
-	$('#js-run-stop')
-		.addClass('enabled')
-		.prop('disabled', false);
+Vue.component('workspace-generate', {
+  template: '#workspace-generate-tpl',
+});
+
+Vue.component('workspace-log', {
+  template: '#workspace-log-tpl',
+  data: function () {
+    return { text: '' }
+  },
+  methods: {
+    updateScrollTop: function () {
+      this.$el.scrollTop = this.$el.offsetHeight;
+    }
+  },
+  events: {
+    'log-change': function (str) {
+      this.text += str;
+
+      // Wait for Vue to update the DOM
+      // before calling updateScrollTop().
+      this.$nextTick(function () {
+        this.updateScrollTop();
+      });
+    }
+  }
+});
+
+
+// COMPONENTS
+//
+
+Vue.component('modal', {
+  template: '#modal-tpl',
+});
+
+
+// APP
+//
+
+var app = new Vue({
+  el: '#app',
+
+  data: {
+    state: state,
+    collider: {
+      version: pkg.version
+    },
+  },
+
+  methods: {
+    send: ipcRenderer.send
+  },
+});
+
+
+
+// EVENTS
+//
+
+// Fired on:
+// - New, and
+// - Open
+ipcRenderer.on('did-open-project', function (e, dir, data) {
+	state.isLoaded = true;
+  state.location = {
+    rel: tildify(dir),
+    abs: dir,
+  };
+  state.project = data;
 });
 
 ipcRenderer.on('did-run-project', function (e, args) {
-	$('#js-run-stop').text('Stop');
+	state.isRunning = true;
 });
 
 ipcRenderer.on('did-stop-project', function (e, args) {
-	$('#js-run-stop').text('Run');
+	state.isRunning = false;
 });
 
-// Console
-ipcRenderer.on('consoleOut', function (e, str) {
-	console += str;
-	$('#js-console').text(console);
+// Log.
+ipcRenderer.on('logOut', function (e, str) {
+  app.$broadcast('log-change', str);
 });
 
-ipcRenderer.on('consoleErr', function (e, str) {
-	console += str;
-	$('#js-console').text(console);
+ipcRenderer.on('logErr', function (e, str) {
+  app.$broadcast('log-change', str);
 });
--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 'use strict';
 
+var collider = require('./lib/collider');
+var cmds = require('./lib/commands');
+
 // Electron
 var electron = require('electron');
 var app      = electron.app;
 var ipcMain  = electron.ipcMain;
 var dialog   = electron.dialog;
+
+var os = require('os');
+var path = require('path');
 
 require('electron-debug')();
 require('fix-path')();
@@ -44,34 +50,87 @@ app.on('activate', function () {
 app.on('ready', function () {
 	mainWindow = createMainWindow();
 
+
 	// Collider
 	//
 
-	var projectDirPath = '';
+	var projectDir = '';
 	var projectRunning = false;
 
 	var gulp;
+
 
 	// EVENTS
 	//
 
 	ipcMain.on('project-open', function () {
 		dialog.showOpenDialog(mainWindow, { properties: ['openDirectory'] }, function (dir) {
-			projectDirPath = dir;
 
-			// If projectDirPath has a collider.json file, then emit
-			// event and send contents of collider.json file also.
-			mainWindow.webContents.send('did-open-project', dir);
+      // If a dir. wasn't selected, don't
+      // attempt to load 'collider.json'.
+      if (!dir) return;
+
+      // Reduce 'dir' from array to string.
+      dir = dir[0];
+
+      collider.load(dir, function (err, data) {
+        if (!err) {
+          projectDir = dir;
+          mainWindow.webContents.send('did-open-project', projectDir, data);
+        } else {
+          dialog.showMessageBox(mainWindow, {
+            type: 'error',
+            buttons: ['Okay'],
+            defaultId: 0,
+            message: 'Sorry, there was a problem opening that.',
+            detail: 'Please make sure it was a Collider project.',
+          });
+        }
+      });
 		});
 	});
+
+  ipcMain.on('project-new', function () {
+    dialog.showSaveDialog(mainWindow, {
+      defaultPath: os.homedir(),
+      buttonLabel: 'Create'
+    }, function (filename) {
+
+      // If user clicked cancel, do nothing.
+      if (!filename) return;
+
+      // To do: verify 'filename' is a valid directory
+      //        name before setting 'newProjectDir'.
+      var newProjectDir = filename;
+      var newProjectDirParsed = path.parse(newProjectDir);
+
+      cmds.new({
+        name: newProjectDirParsed.name,
+        dir: newProjectDirParsed.dir,
+        author: 'Unknown',
+        matterLibs: [],
+      }, function (err) {
+        if (err) throw err;
+
+        collider.load(newProjectDir, function (err, data) {
+          if (err) throw err;
+
+          // Update 'projectDir' and open
+          // the new project in the GUI.
+          projectDir = newProjectDir;
+          mainWindow.webContents.send('did-open-project', projectDir, data);
+        });
+      });
+    });
+  })
 
 	ipcMain.on('project-run-stop', function () {
 		if (projectRunning) {
 			gulp.kill();
 		} else {
 			gulp = spawn(
-				`${projectDirPath}/node_modules/.bin/gulp`,
-				['--cwd', projectDirPath, '--no-color', 'default']
+				`${projectDir}/node_modules/.bin/gulp`,
+				['--cwd', projectDir, '--no-color', 'default']
 			);
 
 			projectRunning = true;
@@ -79,15 +138,15 @@ app.on('ready', function () {
 		}
 
 		gulp.stdout.on('data', function (data) {
-			mainWindow.webContents.send('consoleOut', data.toString());
+			mainWindow.webContents.send('logOut', data.toString());
 		});
 
 		gulp.stderr.on('data', function (data) {
-			mainWindow.webContents.send('consoleErr', data.toString());
+			mainWindow.webContents.send('logErr', data.toString());
 		});
 
 		gulp.on('error', function (err) {
-			mainWindow.webContents.send('consoleErr', err);
+			mainWindow.webContents.send('logErr', err);
 		});
 
 		gulp.on('close', function (code, signal) {
